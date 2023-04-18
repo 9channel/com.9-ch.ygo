1b78eb5dabd55322385713867d85df9cfc17708d
RyoLee <516127941@qq.com>
2023-04-18 22:02:57 +0800

    temp: save

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
+      path: "/",
+      name: "home",
+      component: () => import("@/views/HomeView.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: "/about",
+      name: "about",
+      component: () => import("@/views/AboutView.vue"),
     },
     {
-      path: '/swpf',
-      name: 'swpf',
-      component: () => import('../views/SwpfView.vue')
+      path: "/swpf",
+      name: "swpf",
+      component: () => import("@/views/SwpfView.vue"),
     },
     {
-      path: '/ttl',
-      name: 'ttl',
-      component: () => import('../views/TtlView.vue')
-    }
-  ]
-})
+      path: "/ttl",
+      name: "ttl",
+      component: () => import("@/views/TtlView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
--- updateCards.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ./public/
-rm -f cards.zip cards.zip.md5 cards.json
-curl https://ygocdb.com/api/v0/cards.zip.md5 -o cards.zip.md5
-curl https://ygocdb.com/api/v0/cards.zip -o cards.zip
-unzip cards.zip
-rm -f cards.zip
-cd ../../
